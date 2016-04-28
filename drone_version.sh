8a93ea3fedcf8d350e9f5dceb76bfefcdc623e53
Logan Dethrow <log@lindenlab.com>
2016-04-28 18:58:34 +0000

    Changed sed option.

--- drone_version.sh
+++ drone_version.sh
@@ -10,13 +10,16 @@ if [[ $DRONE ]]; then
 
     COMMIT_ID="$(git log --pretty=format:'%h' -n 1)" 
     NOW="$(date +'%Y-%m-%dT%H:%M:%SZ')"
+    # This requires using v<X>.<Y>.<Z> style version numbers in the tags of 
+    # commits. For now, all backend services will be 0.0.0. commitID is more 
+    # important for our purposes anyway. 
     VERSION=$(git tag --contains $DRONE_COMMIT | grep "$DRONE_BRANCH" | tail -n1 | awk -F'v' '{print $2}') 
     if [[ ! $VERSION =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
         export VERSION="0.0.0";
     fi
     export CGO_ENABLED=0
     echo "commit_id:$COMMIT_ID, now:$NOW, version:$VERSION, cgo:$CGO_ENABLED, drone_commit:$DRONE_COMMIT, drone_build_number:$DRONE_BUILD_NUMBER, drone_branch:$DRONE_BRANCH, drone_image:$DRONE_DOCKER_IMAGE"
-    sed -i .bak \
+    sed -i \
         -e 's/    softwareVersion string = *$/    softwareVersion string = \"$VERSION\"/' \
         -e 's/    commitID string = *$/    commitID string = \"$COMMIT_ID\"/' \
         -e 's/    buildID string = *$/    buildID string = \"$DRONE_BUILD_NUMBER\"/' \
