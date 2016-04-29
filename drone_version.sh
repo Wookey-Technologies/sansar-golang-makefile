065bf484e9bea087ef9034208be6b7089ad4e58f
Logan Dethrow <log@lindenlab.com>
2016-04-29 23:53:09 +0000

    Fixed sed wildcard and escaping.

--- drone_version.sh
+++ drone_version.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+set -e
+set -x
+
 if [[ ! -a version.go ]]; then
     # Service doesn't support info/version
     echo "Skipped saving version info because the service doesn't support info endpoint." 
@@ -19,14 +22,17 @@ if [[ $DRONE ]]; then
     fi
     export CGO_ENABLED=0
     echo "commit_id:$COMMIT_ID, now:$NOW, version:$VERSION, cgo:$CGO_ENABLED, drone_commit:$DRONE_COMMIT, drone_build_number:$DRONE_BUILD_NUMBER, drone_branch:$DRONE_BRANCH, drone_image:$DRONE_DOCKER_IMAGE"
+    # escape '/' in image to '\/'
+    DOCKER_IMAGE_FOR_SED=$(echo $DRONE_DOCKER_IMAGE | sed 's/\//\\\//g')
     sed -i \
-        -e 's/    softwareVersion string = *$/    softwareVersion string = \"$VERSION\"/' \
-        -e 's/    commitID string = *$/    commitID string = \"$COMMIT_ID\"/' \
-        -e 's/    buildID string = *$/    buildID string = \"$DRONE_BUILD_NUMBER\"/' \
-        -e 's/    branch string = *$/    branch string = $DRONE_BRANCH/' \
-        -e 's/    image string = *$/    image string = registry.docker\/$DRONE_DOCKER_IMAGE/' \
-        -e 's/    buildTime string = *$/    buildtime string = \"$NOW\"/' \
+        -e "s/const softwareVersion string = .*$/const softwareVersion string = \""$VERSION"\"/" \
+        -e "s/const commitID string = .*$/const commitID string = \""$COMMIT_ID"\"/" \
+        -e "s/const buildID string = .*$/const buildID string = \""$DRONE_BUILD_NUMBER"\"/" \
+        -e "s/const branch string = .*$/const branch string = \""$DRONE_BRANCH"\"/" \
+        -e "s/const image string = .*$/const image string = \""$DOCKER_IMAGE_FOR_SED"\"/" \
+        -e "s/const buildTime string = .*$/const buildTime string = \""$NOW"\"/" \
     version.go
+    cat version.go
 else
     echo "using default version.go, since this is a local build."
 fi
